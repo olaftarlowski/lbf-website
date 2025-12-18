e4f0db1d2961c2add25419e57fa8538019a7af91
olfiHP <olaf.tarlowski@gmail.com>
2025-12-18 12:50:19 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Home from './pages/Home'
-import useMobile from './components/hooks/useMobile'
+import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import useMobile from './components/hooks/useMobile'
 
 function App() {
-  const [count, setCount] = useState(0)
-   const isMobile = useMobile();
+  // const [count, setCount] = useState(0)
+  //  const isMobile = useMobile();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
       <Home />
-      {isMobile ? <p>r</p> : <p>wienksze</p>}
+      {/* {isMobile ? <p>r</p> : <p>wienksze</p>} */}
     </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import {
+  bannerMain1920,
+  // bannerMain1440,
+  bannerMain720,
+  // bannerTavern1920,
+} from "../assets";
+import useMobile from "../components/hooks/useMobile";
 
 const Home = () => {
-  return (
-    <div>Homeeee</div>
-  )
-}
+  const isMobile = useMobile();
 
-export default Home
+  return (
+    <div>
+      <h1>Limit Breakers Familia</h1>
+      <p>UNDER CONSTRUCTION</p>
+      {isMobile ? (
+        <img
+          className="test-img"
+          src={bannerMain720}
+          alt="fist breaking through the screen"
+        />
+      ) : (
+        <img
+          className="test-img"
+          src={bannerMain1920}
+          alt="fist breaking through the screen"
+        />
+      )}
+
+      {/* <img className="test-img" src={bannerMain1440} alt="fist breaking through the screen" /> */}
+
+      {/* <img className="test-img" src={bannerTavern1920} alt="fist breaking through the screen" /> */}
+    </div>
+  );
+};
+
+export default Home;
